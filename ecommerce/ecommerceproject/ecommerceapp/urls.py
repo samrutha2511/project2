a3b33485ba816a58f37bc92b9cf0dce3258a40db
amrutha <amrutha@gmail.com>
2022-10-31 10:01:41 +0530

    save

---
+++ ecommerce/ecommerceproject/ecommerceapp/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+app_name='ecommerceapp'
+
+urlpatterns = [
+    # path('',views.demo, name='demo'),
+    path('',views.allProdCat, name='allProdCat'),
+    path('<slug:c_slug>/',views.allProdCat,name='products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>', views.proDetails, name='allCatdetails'),
+
+]
